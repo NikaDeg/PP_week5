28098b17924b0fa6bb513b52de23aca216f60c86
Aakash-Basnet <aakashb@metropolia.fi>
2026-09-16 14:19:05 +0300

    [iter2] implement job fetching on homepage

--- week5-fepp-starter/frontend/src/pages/HomePage.jsx
+++ week5-fepp-starter/frontend/src/pages/HomePage.jsx
@@ -1,19 +1,25 @@
-import JobListing from "../components/JobListing";
-import { useEffect, useState } from "react";
+import JobListing from '../components/JobListing';
+import { useEffect, useState } from 'react';
 
 const Home = () => {
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const jobs = await fetch('/api/jobs');
+      const json = await jobs.json();
+      setJobs((prev) => (prev, json));
+    };
+    fetchJobs();
+  }, []);
   const [jobs, setJobs] = useState([]);
 
   return (
     <div className="home">
       <div className="job-list">
         {jobs.length === 0 && <p>No jobs found</p>}
-        {jobs.length !== 0 &&
-          jobs.map((job) => <JobListing key={job.id} {...job} />)}
+        {jobs.length !== 0 && jobs.map((job) => <JobListing key={job.id} {...job} />)}
       </div>
     </div>
   );
 };
 
 export default Home;
-
